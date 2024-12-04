09731c4e1ee487f41d3642744ba58c4171278992
DannyGonzalezO <dannygonzalez1327@hotmail.com>
2024-12-04 16:17:50 -0300

    se agrega funcion de buscador

--- src/Views/Index.jsx
+++ src/Views/Index.jsx
@@ -10,10 +10,17 @@ function Index() {
     // Se definen las variables offset y limit, definiendo un limite de 20 Pokemon por página.
     const [offset, setOffset] = useState(0);
     const [limit, setLimit] = useState(20);
+    // Se define la variable allPokemon, que almacena todos los Pokemon.
+    const [allPokemon, setAllPokemon] = useState([]);
+    // Se define la variable listado, que almacena los Pokemon que se mostrarán en la página.
+    const [listado, setListado] = useState([]);
+    // Se define la variable filtro, que guarda el filtro de búsqueda.
+    const [filtro, setFiltro] = useState('');
 
     // Se llama a la función getPokemon cada que se carga la página.
     useEffect(() => {
         getPokemon(offset);
+        getAllPokemon();
     }, []);
 
     // Se define getPokemon, que obtiene los Pokemon que se mostrarán en la página.
@@ -24,8 +31,39 @@ function Index() {
             const respuesta = response.data;
             // Se almacena la respuesta en la variable pokemon.
             setPokemon(respuesta.results);
-    })
-}
+            // Se almacena la respuesta en la variable listado.
+            setListado(respuesta.results);
+        })
+    }
+
+    // Se define getAllPokemon, que obtiene todos los Pokemon.
+    const getAllPokemon = async () => {
+        const fuente = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=100000`;
+        // Se obtiene la respuesta de la API.
+        axios.get(fuente).then( async (response) => {
+            const respuesta = response.data;
+            // Se almacena la respuesta en la variable pokemon.
+            setAllPokemon(respuesta.results);
+        })
+    }
+
+    //se define buscarPokemon, que filtra los Pokemon que se mostrarán en la página.
+    const buscarPokemon = (e) => {
+        if (e.keyCode == 13) {
+            if(filtro.trim() != ''){
+                setListado([]);
+                setTimeout(() => {
+                    setListado(allPokemon.filter(p => p.name.includes(filtro)));
+                }, 100)
+            }
+        } else if(filtro.trim() == '') {
+            setListado([]);
+            setTimeout(() => {
+                setListado(allPokemon);
+            }, 100);
+        }
+        }
+
   return (
     <Container className="shadow bg-danger mt-3">
       <Row>
@@ -37,12 +75,12 @@ function Index() {
         <Col>
           <InputGroup className="mt-3 mb-3 shadow">
             <InputGroupText><i className="fa-solid fa-search"></i></InputGroupText>
-            <Input placeholder="Buscar Pokémon"></Input>
+            <Input value={filtro} onChange={(e) => {setFiltro(e.target.value)}} onKeyUpCapture={buscarPokemon} placeholder="Buscar Pokémon"></Input>
           </InputGroup>
         </Col>
       </Row>
       <Row className="mt-3">
-        {pokemon.map((p,i) => (
+        {listado.map((p,i) => (
         <PokemonBox name={p.name} key={i} />
         ))}
       </Row>
